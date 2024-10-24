5191c60d7dee9f87c4569c5b05f8eb8901ab6bf6
Redheart <battlefield980@mail.ru>
2024-10-24 18:34:30 +0500

    Задание 12.
    Малыгин Никита. Задание 12.

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -12,7 +12,17 @@ public class Task12Main {
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr != null && arr.length > 0) {
+            for (int i = 0; i < arr.length; i++) {
+                for (int min = arr[i], j = i; j < arr.length; j++) {
+                    if (min > arr[j]) {
+                        min = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = min;
+                    }
+                }
+            }
+        }
     }
 
 }
